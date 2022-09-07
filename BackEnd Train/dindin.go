93f27c08255b67a4dc9b38b991d0ea7213dd8e3a
Dindin Inas Candra Wiguna <62365695+Dindin14@users.noreply.github.com>
2022-09-07 18:02:36 +0700

    Update dindin.go

--- BackEnd Train/dindin.go
+++ BackEnd Train/dindin.go
@@ -89,6 +89,8 @@ type Karyawan struct {
 func main() {
 	var point int
 	var makans []Makanan
+	var minums []Minuman
+	var karyawans []Karyawan
 
 	//Koneksi Ke Data Base
 	dsn := "dindin:PzcAyvPNrF@tcp(db.kaenova.my.id:2003)/dindin?charset=utf8mb4&parseTime=True&loc=Local"
@@ -121,7 +123,13 @@ func main() {
 	fmt.Println("4. Delete Makanan")
 	fmt.Println("5. Delete Minuman")
 	fmt.Println("6. Delete Karyawan")
-	fmt.Println("9. Exit")
+	fmt.Println("7. Update Makanan")
+	fmt.Println("8. Update Minuman")
+	fmt.Println("9. Update Karyawan")
+	fmt.Println("10. Lihat Data Makanan")
+	fmt.Println("11. Lihat Data Minuman")
+	fmt.Println("12. Lihat Data Karyawan")
+	fmt.Println("99. Exit")
 
 	fmt.Scanln(&point)
 	switch point {
@@ -183,40 +191,114 @@ func main() {
 		}
 	case 7:
 
+		//Update data nama Makanan dan Harga
 		var update, hargaUpdate int
 		var namaUpdate, idUptMakanan string
 
-		//Update data Makanan
 		// makans := Makanan{Nama: "Bakso", Harga: 10000}
 		// makans.Nama = "Wacepong"
 
 		fmt.Println("ID Makanan: ")
 		fmt.Scanln(&idUptMakanan)
-		// idUptMakanan := pkgmakanan.UpdateMenuMakanan()
-		fmt.Println("Ganti Nama Makanan")
-		fmt.Println("Ganti Harga Makanan")
+		fmt.Println("1. Ganti Nama Makanan")
+		fmt.Println("2. Ganti Harga Makanan")
 		fmt.Scanln(&update)
 
 		switch update {
 		case 1:
+			fmt.Println("Nama Makanan: ")
 			fmt.Scanln(&namaUpdate)
 			db.Model(&Makanan{}).Where("id = ?", idUptMakanan).Update("Nama", namaUpdate)
 		case 2:
-			fmt.Scanln(&namaUpdate)
+			fmt.Println("Harga Makanan: ")
+			fmt.Scanln(&hargaUpdate)
 			db.Model(&Makanan{}).Where("id = ?", idUptMakanan).Update("Harga", hargaUpdate)
 		}
-
 	case 8:
 
-		//Melihat Data
+		//Update data Nama Minuman,Toping, dan Harga
+		var update, hargaUpdate int
+		var namaUpdate, topingUpdate, idUptMinuman string
+
+		fmt.Println("ID Minuman: ")
+		fmt.Scanln(&idUptMinuman)
+		fmt.Println("1. Ganti Nama Minuman")
+		fmt.Println("2. Ganti Toping Minuman")
+		fmt.Println("3. Ganti Harga Minuman")
+		fmt.Scanln(&update)
+
+		switch update {
+		case 1:
+			fmt.Println("Nama Minuman: ")
+			fmt.Scanln(&namaUpdate)
+			db.Model(&Minuman{}).Where("id = ?", idUptMinuman).Update("Nama", namaUpdate)
+		case 2:
+			fmt.Println("Toping Minuman: ")
+			fmt.Scanln(&topingUpdate)
+			db.Model(&Minuman{}).Where("id = ?", idUptMinuman).Update("Toping", topingUpdate)
+		case 3:
+			fmt.Println("Harga Minuman: ")
+			fmt.Scanln(&hargaUpdate)
+			db.Model(&Minuman{}).Where("id = ?", idUptMinuman).Update("Harga", hargaUpdate)
+		}
+	case 9:
+
+		//Update data Nama Karyawan,Umur, dan Asal
+		var update, umurUpdate int
+		var namaUpdate, asalUpdate, idUptKaryawan string
+		fmt.Println("ID Karyawan: ")
+		fmt.Scanln(&idUptKaryawan)
+		fmt.Println("1. Ganti Nama Karyawan")
+		fmt.Println("2. Ganti Umur Karyawan")
+		fmt.Println("3. Ganti Asal Karyawan")
+		fmt.Scanln(&update)
+
+		switch update {
+		case 1:
+			fmt.Println("Nama Karyawan: ")
+			fmt.Scanln(&namaUpdate)
+			db.Model(&Karyawan{}).Where("id = ?", idUptKaryawan).Update("Nama", namaUpdate)
+		case 2:
+			fmt.Println("Umur Karyawan: ")
+			fmt.Scanln(&umurUpdate)
+			db.Model(&Karyawan{}).Where("id = ?", idUptKaryawan).Update("Umur", umurUpdate)
+		case 3:
+			fmt.Println("Asal Karyawan: ")
+			fmt.Scanln(&asalUpdate)
+			db.Model(&Karyawan{}).Where("id = ?", idUptKaryawan).Update("Asal", asalUpdate)
+		}
+	case 10:
+
+		//Melihat Data Makanan
 		result := db.Find(&makans)
 		err = result.Error
 		if err != nil {
 			panic(err.Error())
 		}
 		fmt.Println(makans)
-	case 9:
-		os.Exit(9)
+
+	case 11:
+
+		//Melihat Data Minuman
+		result := db.Find(&minums)
+		err = result.Error
+		if err != nil {
+			panic(err.Error())
+		}
+		fmt.Println(minums)
+
+	case 12:
+
+		//Melihat Data Karyawan
+		result := db.Find(&karyawans)
+		err = result.Error
+		if err != nil {
+			panic(err.Error())
+		}
+		fmt.Println(karyawans)
+
+	case 99:
+		os.Exit(99)
 	}
 
 	//Ambil Data
@@ -259,7 +341,3 @@ func main() {
 	// fmt.Println(makan2)
 
 }
-
-// func TambahMakanan() {
-// 	panic("unimplemented")
-// }
