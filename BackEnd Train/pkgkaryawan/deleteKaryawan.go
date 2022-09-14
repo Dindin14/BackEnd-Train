04010aa3ce6e29943f0f993fd401e74c849cdc7a
Dindin Inas Candra Wiguna <62365695+Dindin14@users.noreply.github.com>
2022-09-14 19:12:53 +0700

    Update deleteKaryawan.go

--- BackEnd Train/pkgkaryawan/deleteKaryawan.go
+++ BackEnd Train/pkgkaryawan/deleteKaryawan.go
@@ -1,12 +1,19 @@
 package pkgkaryawan
 
-import "fmt"
+import (
+	"fmt"
+	"gormstudy/db"
+)
 
 var idKaryawan string
 
-func KurangKaryawan() string {
+func KurangKaryawan() {
 	fmt.Println("ID Karyawan: ")
 	fmt.Scanln(&idKaryawan)
 
-	return idKaryawan
+	result := db.DB.Delete(&db.Karyawan{}, idKaryawan)
+	err := result.Error
+	if err != nil {
+		panic(err.Error())
+	}
 }
