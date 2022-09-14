de0701f22faf9c2135f7be88c6b8e2f4f37a535f
Dindin Inas Candra Wiguna <62365695+Dindin14@users.noreply.github.com>
2022-09-14 19:11:31 +0700

    Update updateMakanan.go

--- BackEnd Train/pkgmakanan/updateMakanan.go
+++ BackEnd Train/pkgmakanan/updateMakanan.go
@@ -1,13 +1,29 @@
 package pkgmakanan
 
-import "fmt"
+import (
+	"fmt"
+	"gormstudy/db"
+)
 
-func UpdateMenuMakanan() string {
+func UpdateMenuMakanan() {
 
-	var sesuatu string
+	var update, hargaUpdate int
+	var namaUpdate, idUptMakanan string
+
 	fmt.Println("ID Makanan: ")
-	fmt.Scanln("%s", &sesuatu)
-	fmt.Println("asdasd")
+	fmt.Scanln(&idUptMakanan)
+	fmt.Println("1. Ganti Nama Makanan")
+	fmt.Println("2. Ganti Harga Makanan")
+	fmt.Scanln(&update)
 
-	return sesuatu
+	switch update {
+	case 1:
+		fmt.Println("Nama Makanan: ")
+		fmt.Scanln(&namaUpdate)
+		db.DB.Model(&db.Makanan{}).Where("id = ?", idUptMakanan).Update("Nama", namaUpdate)
+	case 2:
+		fmt.Println("Harga Makanan: ")
+		fmt.Scanln(&hargaUpdate)
+		db.DB.Model(&db.Makanan{}).Where("id = ?", idUptMakanan).Update("Harga", hargaUpdate)
+	}
 }
