b238d963df1d45c43be1f1cd616165ffd2a91075
Dindin Inas Candra Wiguna <62365695+Dindin14@users.noreply.github.com>
2022-09-14 19:12:26 +0700

    Update addMakanan.go

--- BackEnd Train/pkgmakanan/addMakanan.go
+++ BackEnd Train/pkgmakanan/addMakanan.go
@@ -1,15 +1,21 @@
 package pkgmakanan
 
-import "fmt"
+import (
+	"gormstudy/db"
+)
 
-var namaMakanan string
-var hargaMakanan int
+// var namaMakanan string
+// var hargaMakanan int
 
-func TambahMakanan() (string, int) {
-	fmt.Println("Nama makanan: ")
-	fmt.Scanln(&namaMakanan)
-	fmt.Println("Harga: ")
-	fmt.Scanln(&hargaMakanan)
+func TambahMakanan(namaMakanan string, hargaMakanan int) {
 
-	return namaMakanan, hargaMakanan
+	// fmt.Println("Nama makanan: ")
+	// fmt.Scanln(&namaMakanan)
+	// fmt.Println("Harga: ")
+	// fmt.Scanln(&hargaMakanan)
+	result := db.DB.Create(&db.Makanan{Nama: namaMakanan, Harga: hargaMakanan})
+	err := result.Error
+	if err != nil {
+		panic(err.Error())
+	}
 }
